1b3f86ebcee8474cefbd5dc8217698b126b03dc3
Logan Schorr <logan.schorr89@gmail.com>
2023-03-29 13:36:30 -0400

    Updating organization for remote development

--- build/vcu_ur_driver/build/lib/vcu_ur_driver/ur_driver.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from vcu_ur_driver.')
-
-
-if __name__ == '__main__':
-    main()
--- install/am_vision/lib/python3.10/site-packages/am_vision/pointcloud_capture.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from am_vision.')
-
-
-if __name__ == '__main__':
-    main()
